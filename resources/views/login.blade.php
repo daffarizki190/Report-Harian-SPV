9684421095654165369ac79ff8cda56e80a35b9a
daffarizki190 <dafarizki209@gmail.com>
2026-05-04 11:41:16 +0700

    design: update copyright year to 2026

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -85,7 +85,7 @@
 
         <div class="mt-12 flex flex-col items-center gap-3 opacity-60">
             <p class="text-white text-[10px] font-black tracking-[0.4em] uppercase">Parking Management System</p>
-            <p class="text-white/70 text-[10px] font-bold uppercase tracking-widest">© 2024 Crafted with Excellence by Daffa Rizki Ariyanto</p>
+            <p class="text-white/70 text-[10px] font-bold uppercase tracking-widest">© 2026 Crafted with Excellence by Daffa Rizki Ariyanto</p>
         </div>
     </div>
 
